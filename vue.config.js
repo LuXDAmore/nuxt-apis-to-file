ce95205736e750a3e71d9fc9edb6d19e7d46fa86
Luca Iaconelli <lucabelli@msn.com>
2020-01-08 17:15:12 +0100

    chore: updated vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,37 @@
 // Exports
 module.exports = {
     lintOnSave: true,
+    productionSourceMap: false,
+    chainWebpack(
+        config,
+    ) {
+
+        config.resolve.symlinks(
+            false,
+        );
+
+        config
+            .module
+            .rule(
+                'vue',
+            )
+            .use(
+                'vue-loader',
+            )
+            .loader(
+                'vue-loader',
+            )
+            .tap(
+                options => {
+
+                    options.compilerOptions.preserveWhitespace = false;
+                    options.compilerOptions.whitespace = 'condense';
+
+                    return options;
+
+                },
+            )
+        ;
+
+    },
 };
